c17e3cafdfcdfd602204430f873a2281d19abb01
738NGX <ningyue@738ngx.site>
2023-11-16 00:15:03 +0800

    2311160014

--- RinaChanBoardOperationCenter/pages/music/music.js
+++ RinaChanBoardOperationCenter/pages/music/music.js
@@ -2,31 +2,13 @@ const app=getApp();
 
 const face_func=require('../../utils/face_func.js');
 const util=require('../../utils/util.js');
+const audio = wx.createInnerAudioContext();
 
 const{music_data}=require('../../utils/music_data.js');
 
 function sleep (time) 
 {
     return new Promise((resolve) => setTimeout(resolve, time));
-}
-
-async function play_face(obj,id)
-{
-    const using_faces=music_data[id].faces;
-    for(let i=0;i<using_faces.length;i++)
-    {
-        face_func.set_face(obj,using_faces[i].leye,using_faces[i].reye,using_faces[i].mouth,using_faces[i].cheek); 
-        await sleep(using_faces[i].during*1000/30.0);
-    }
-}
-
-function play_audio(src)
-{
-    const audio=wx.createInnerAudioContext();
-    audio.autoplay=false;
-    audio.src=src;
-    console.log('Playing '+src)
-    audio.play();
 }
 
 Page({
@@ -38,6 +20,12 @@ Page({
         music_texts: music_data.map(music => music.text),
         cover_srcs: music_data.map(music => music.cover_src),
         choosing_music: 0,
+
+        playing:false,
+
+        current_frame:0,
+        current_face:0,
+        frames:1,
     },
     onLoad() 
     { 
@@ -46,6 +34,7 @@ Page({
         app.addCellsListener((newCells) => {
             this.setData({ cells: newCells });
         });
+        audio.autoplay=false;
     },
     onUnload() {
         // 移除监听器
@@ -53,32 +42,58 @@ Page({
     },
     bindPickerChange: function(e) 
     {
-        this.setData({choosing_music: parseInt(e.detail.value)});
-    },
-    play_music()
-    {
-        console.log(this.data.choosing_music);
-        var src=music_data.map(music => music.music_src)[this.data.choosing_music];
-        play_audio(src);
-        
-        wx.request({
-            url: 'https://api.bemfa.com/api/device/v1/data/1/',
-            method: "POST",
-            data:
-            {
-                uid: util.device_info[app.get_controlling_device()].uid,
-                topic: util.device_info[app.get_controlling_device()].topic,
-                msg: music_data.map(music => music.id)[this.data.choosing_music],
-            },
-            header:
-            {
-                'content-type': "application/x-www-form-urlencoded"
-            },
-            success(res) {
-                console.log(res.data);
-            }
+        let idx=parseInt(e.detail.value);
+        const using_faces=music_data[idx].faces;
+        this.setData({
+            choosing_music: idx,
+            frames:using_faces[using_faces.length-1].frame-1,
+            current_frame:0,
         });
-        
-        play_face(this,this.data.choosing_music);
+        audio.src=music_data.map(music=>music.music_src)[this.data.choosing_music];
+        face_func.set_face(this,using_faces[0].leye,using_faces[0].reye,using_faces[0].mouth,using_faces[0].cheek); 
     },
+    sliderChange: function(e)
+    {
+        f=parseInt(e.detail.value);
+        this.setData({current_frame:f});
+        const using_faces=music_data[this.data.choosing_music].faces;
+        audio.seek(this.data.current_frame/10);
+        for(let i=0;i<using_faces.length;i++)
+        {
+            if(this.data.current_frame<music_data[this.data.choosing_music].faces[i].frame)
+            {
+                this.setData({current_face:i-1});
+                face_func.set_face(this,using_faces[i-1].leye,using_faces[i-1].reye,using_faces[i-1].mouth,using_faces[i-1].cheek);
+                break;
+            }
+        }
+    },
+    async play_music()
+    {
+        this.setData({playing:true});
+        const using_faces=music_data[this.data.choosing_music].faces;
+        audio.play();
+        while(this.data.playing&&this.data.current_frame<this.data.frames)
+        {
+            for(let i=0;i<using_faces.length;i++)
+            {
+                if(this.data.current_frame<music_data[this.data.choosing_music].faces[i].frame)
+                {
+                    if(i-1==this.data.current_face) break;
+                    this.setData({current_face:i-1});
+                    face_func.set_face(this,using_faces[i-1].leye,using_faces[i-1].reye,using_faces[i-1].mouth,using_faces[i-1].cheek);
+                    break;
+                }
+            }
+            var next_frame=(Math.abs(Math.floor(audio.currentTime*10)-this.data.current_frame)>3)?Math.floor(audio.currentTime*10):this.data.current_frame+1;
+            this.setData({current_frame:next_frame});
+            await sleep(100);
+        }
+        this.setData({playing:false});
+    },
+    async stop_music()
+    {
+        this.setData({playing:false});
+        audio.pause();
+    }
 })
